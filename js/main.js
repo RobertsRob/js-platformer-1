aa4048fd988628469d78ef8ddce6d5557e534ed2
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2023-04-22 11:15:50 +0000

    changes

--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
 // "use strict"
 
 var ctx = myCanvas.getContext("2d"); 
-let fps = 0;
-let fpsC = 0;
+let fps = 250;
+let fpsC = 250;
 let r = false;
 let l = false;
 let jump = false;
-let dforce = 100;//30
+let dforce = 30;//30
 let force = dforce;
 let canJump = true;
 
 let gameY = 0;
+let previosPlatformCords = 200;
+const platformDistance = 250;
+let platforms = [];
 
+let cW = myCanvas.width;
+
+for (let i = 0; i < Math.round(myCanvas.height/platformDistance); i++)
+{
+    platforms.push(new GameObject(Math.floor(Math.random() * (cW-150 - 150 + 1) ) + 150, myCanvas.height-(previosPlatformCords+150)+gameY, 150, 50, "grey"));
+    previosPlatformCords += platformDistance;
+}
 
 ctx.fillText( "Loading", 10, 30 );
 
 function start() {
     game = new Game();
     setInterval(update, 25);//25
-    setInterval(updateFast, 1);//1
+    setInterval(updateFast, 0.07);//1//0.1//0.07
 }
 
-let player = new Player(1, 1,  100, 300);
+let player = new Player(myCanvas.width/2, myCanvas.height-150,  100, 100);
 // let platforms = []
 // for (let i = 0; i < array.length; i++)
 // {    
 // }
 
 let platform = new GameObject(0, myCanvas.height-50+gameY, myCanvas.width, 50, "blue");
-let platformSmall = new GameObject(150, myCanvas.height-900+gameY, 60, 50, "grey");
+// let platformSmall = new GameObject(150, myCanvas.height-900+gameY, 60, 50, "green");
 // let platformSmall = new GameObject(150, myCanvas.height-250, 60, 50, "grey");
 
 function update()
@@ -42,36 +52,74 @@ function updateFast()
     let posPl = player.getPos();
     let sizePl = player.getSize();
     let speedPl = player.getSpeed();
-    let posSm = platformSmall.getPos();
+    // let posSm = platformSmall.getPos();
     let posPLB = platform.getPos();
-    let heightSm = platformSmall.getSize();
+    // let heightSm = platformSmall.getSize();
 
     //colisions-----------------------------------
-    if(player.collision(platform) == false)
+    if(player.collision(platform) == false && jump == false)
     {
         player.collisionPlatfDetectedNOT();
     }
-    if(player.collision(platformSmall) == false)
-    {
-        player.collisionPlatfDetectedNOT();
+    // if(player.collision(platformSmall) == false && jump == false)
+    // {
+    //     player.collisionPlatfDetectedNOT();
+    // }
+    for (let i = 0; i < platforms.length; i++) {
+        let posPlatform = platforms[i].getPos();
+        if(player.collision(platforms[i]) == false && jump == false)//10
+        {
+            player.collisionPlatfDetectedNOT();
+        }
     }
 
     if(player.collision(platform) == true)
     {
         player.collisionPlatfDetected();
         canJump = true;
+        player.fallZero();
     }
-    if(player.collision(platformSmall) == true && posPl.y+sizePl.h < posSm.y+10)
-    {
-        player.collisionPlatfDetected();
-        canJump = true;
+    // if(player.collision(platformSmall) == true && posPl.y+sizePl.h < posSm.y+15)//10
+    // {
+    //     player.collisionPlatfDetected();
+    //     canJump = true;
+    // }
+
+    for (let i = 0; i < platforms.length; i++) {
+        let posPlatform = platforms[i].getPos();
+        if(player.collision(platforms[i]) == true && posPl.y+sizePl.h < posPlatform.y+15)//10
+        {
+            player.collisionPlatfDetected();
+            canJump = true;
+            player.fallZero();
+        }
     }
     // variables ------------------------------------
     fps++;
     
     // player movement --------------------------
-    if(l == true){player.left();}
-    if(r == true){player.right();}
+
+    if(posPl.x+2+sizePl.w < myCanvas.width)
+    {
+        if(r == true){player.right(fpsC);}
+    }
+    if(posPl.x+2 > 0)
+    {
+        if(l == true){player.left(fpsC);}
+    }
+
+
+    // else
+    // {
+    //     if(posPl.x+2 < 0)
+    //     {
+
+        
+    //     }else{
+    //         if(l == true){player.left(fpsC);}
+    //         if(r == true){player.right(fpsC);}
+    //     }
+    // }
     if(l == false && r == false){player.setSpeed(0, speedPl.vy);}
 
 
@@ -79,10 +127,15 @@ function updateFast()
     if(jump == true)
     {
         canJump = false;
-        player.setPos(posPl.x, posPl.y - force/1.5);
+        if(posPl.y > 600)
+        {
+            player.setPos(posPl.x, posPl.y - force/1.5);
+        }
+        // player.setPos(posPl.x, posPl.y - force/(fpsC/167*2));
         force = force / 1.065;
         if(force < 1)
         {
+            player.postJ();
             force = dforce;
             jump = false;
         }
@@ -90,32 +143,112 @@ function updateFast()
 
     // updates-------------------------------------------
     ctx.clearRect( 0, 0, myCanvas.width, myCanvas.height);
-    player.update();
-    ctx.fillText(fpsC, 10, 30);
+    // ctx.font = "bold 31px Arial";
+    // if(fpsC > 250){ctx.fillStyle = "violet";}
+    // if(fpsC <= 250){ctx.fillStyle = "green";}
+    // if(fpsC <= 180){ctx.fillStyle = "yellow";}
+    // if(fpsC <= 150){ctx.fillStyle = "red";}
+    // ctx.fillText(fpsC, 10, 30);
+    // ctx.fillStyle = "black";
+    // ctx.strokeText(fpsC, 10, 30);
     platform.draw();
     platform.update();
-    platformSmall.draw();
-    platformSmall.update();
+    // platformSmall.draw();
+    // platformSmall.update();
+    player.update(fpsC);
 
-    // screen moving up ----------------------------------
-    if(posPl.y < 120)
+    for (let i = 0; i < platforms.length; i++) {
+        platforms[i].update();
+        platforms[i].draw();
+        
+    }
+
+    //up------------------------------------------------------
+    if(posPl.y < 600)
     {
         if(jump == true)
         {
             gameY += force;
-            platformSmall.setPos(posSm.x, posSm.y+force);
+            // platformSmall.setPos(posSm.x, posSm.y+force);
             platform.setPos(posPLB.x, posPLB.y+force);
+
+            for (let i = 0; i < platforms.length; i++) {
+                let posPlatform = platforms[i].getPos();
+                platforms[i].setPos(posPlatform.x, posPlatform.y+force);
+                if(platforms.length == i+1 && a < 3)
+                {
+                    console.log(1);
+                    a ++;
+                        platforms.push(new GameObject(Math.floor(Math.random() * (cW-150 - 0 + 1) ) + 0, myCanvas.height-(previosPlatformCords+150)+gameY, 150, 50, "grey"));
+                        previosPlatformCords += platformDistance;
+                    // alert("posledni, xaxaxax $sdas");
+                }
+            }
+        }
+    }
+    //down-------------------------------------------------------
+    if(posPl.y > myCanvas.height-100)
+    {
+        let fall = player.fallFF();
+        fall /= 10;
+        gameY -= fall;
+        let fallT = player.fallFaF();
+        if(jump == false && fallT == true)
+        {
+            // platformSmall.setPos(posSm.x, posSm.y-fall);
+            platform.setPos(posPLB.x, posPLB.y-fall);
+
+            for (let i = 0; i < platforms.length; i++) {
+                let posPlatform = platforms[i].getPos();
+                platforms[i].setPos(posPlatform.x, posPlatform.y-fall);
+            }
         }
     }
 
-}
+    //a----------------------------------------------------
+    for (let i = 0; i < platforms.length; i++)
+    {
+        let posPlatform = platforms[i].getPos();
+        if(platforms.length == i+1)
+        {
+            if(posPlatform.y < 300 && jump == false)
+            {
+                a = 0;
+            }
+        }
+    }
 
+
+    // UI-------------------------------
+    ctx.font = "bold 31px Arial";
+    if(fpsC > 250){ctx.fillStyle = "violet";}
+    if(fpsC <= 250){ctx.fillStyle = "green";}
+    if(fpsC <= 180){ctx.fillStyle = "yellow";}
+    if(fpsC <= 150){ctx.fillStyle = "red";}
+    ctx.fillText(fpsC, 10, 30);
+    ctx.fillStyle = "black";
+    ctx.strokeStyle = "black";
+    ctx.strokeText(fpsC, 10, 30);
+
+    ctx.font = "bold 60px Arial";
+    var gradient = ctx.createLinearGradient(0, 0, myCanvas.width, 0);
+    gradient.addColorStop("0", "magenta");
+    gradient.addColorStop("0.5", "blue");
+    gradient.addColorStop("1.0", "red");
+
+    ctx.strokeStyle = gradient;
+    ctx.fillStyle = gradient;
+    ctx.strokeText(Math.round(gameY/10), myCanvas.width/2-5, 60);
+    ctx.fillText(Math.round(gameY/10), myCanvas.width/2-5, 60);
+
+}
+let a = 0;
 let seconds = 0;
 setInterval(function() {
     seconds++;
-    fpsC = fps;
+    fpsC = fps*2;
     fps = 0;
-}, 1000);
+}, 500);
 
 function ButtonsDown(key)
 {
@@ -132,6 +265,7 @@ function ButtonsDown(key)
     {
         // player.addForce();
         jump = true;
+        player.GN();
     }
 }
 function ButtonsUp(key)
--- js/player.js
+++ js/player.js
@@ -3,22 +3,40 @@ class Player extends gameObjPlayer {
     {
         super(x, y, w, h, "red");
 
-        this.fall = false;
+        this.fall = true;
         // this.force = 1000;
-        this.fallF = 4;
-        this.g = 1;
+        this.fallF = 20;//20
+        this.g = 3;//3
     }
-    update()
+    update(fps)
     {
         //gravitation-----------------------------------
         if(this.fall == true)
         {
             let pos = this.getPos();
-            this.setPos(pos.x, pos.y+this.fallF/50);
+            if(this.y < myCanvas.height-100)
+            {
+                this.setPos(pos.x, pos.y+this.fallF/(fps/8));//30
+            }
+            // console.log(this.fallF/(fps/8));
             this.fallF += this.g;
         }
         
-        super.update();
+        //update speed
+        // this.x += this.vx;
+        this.y += this.vy;
+
+        if(this.x+1+this.w < myCanvas.width && this.vx > 0)
+        {
+            // if(r == true){player.right(fpsC);}
+            this.x += this.vx;
+        }
+        if(this.x-1 > 0 && this.vx < 0)
+        {
+            this.x += this.vx;
+            // if(l == true){player.left(fpsC);}
+        }
+
         super.draw();
     }
     detectFallTF()
@@ -39,20 +57,43 @@ class Player extends gameObjPlayer {
         return other.x < this.x+this.w && other.x+other.w > this.x && other.y < this.y+this.h && other.y+other.h > this.y;
     }
 
-    right()
+    right(fps)
     {
         // let pos = this.getPos();
         // this.setPos(pos.x+6, pos.y);
         let speed = this.getSpeed();
-        this.setSpeed(6/3, speed.vy);
+        this.setSpeed(8/2.5, speed.vy);
+        // this.setSpeed(6/(fps/92), speed.vy);
     }
 
-    left()
+    left(fps)
     {
         // let pos = this.getPos();
         // this.setPos(pos.x-6, pos.y);
         let speed = this.getSpeed();
-        this.setSpeed(-6/3, speed.vy);
+        this.setSpeed(-8/2.5, speed.vy);
+        // this.setSpeed(-6/(fps/92), speed.vy);
+    }
+    GN()
+    {
+        this.fall = false;
+    }
+    postJ()
+    {
+        this.fall = true;
+    }
+
+    fallFF()
+    {
+        return this.fallF;
+    }
+    fallFaF()
+    {
+        return this.fall;
+    }
+    fallZero()
+    {
+        this.fallF = 0;
     }
 
 }
